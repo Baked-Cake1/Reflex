70cdb90baa5e3d9ab1320cb8c24c4bc9416c2ef0
Baked-Cake1 <ttheapkdownloader@gmail.com>
2026-03-14 14:51:58 +1100

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,3 @@
+if game.PlaceId == 131623223084840 then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/Baked-Cake1/Reflex/refs/heads/main/scripts/Escape%20Waves%20for%20Brainrots/main.lua'))()
+end
